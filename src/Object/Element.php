5d6b38a2f3c9dd528bf2d7f7376502fda705e08b
Daniel Westerling <daniel@westerling.io>
2020-06-08 16:56:30 +0200

    Added WaitUntil function in Element

--- src/Object/Element.php
+++ src/Object/Element.php
@@ -5,11 +5,9 @@ declare(strict_types=1);
 namespace Akdr\Selma\Object;
 
 use Akdr\Selma\Navigation;
-use Exception;
+use Facebook\WebDriver\WebDriverKeys;
 use Facebook\WebDriver\WebDriverBy;
 use JBZoo\Utils\Filter;
-use ReflectionClass;
-use function array_walk;
 
 class Element {
     /**
@@ -18,7 +16,7 @@ class Element {
     private $navigation;
 
     /**
-     * @var \Facebook\WebDriver\Remote\RemoteWebElement
+     * @var Facebook\WebDriver\Remote\RemoteWebElement
      */
     public $element;
 
@@ -26,6 +24,11 @@ class Element {
      * @var mixed
      */
     private $value;
+
+    /**
+     * @var string
+     */
+    private $selector;
 
     /**
      * @var bool
@@ -37,7 +40,7 @@ class Element {
         $this->navigation = $navigation;
         // Walk through the options array and set variables in the class.
         // Selector must be the first option in the array.
-        array_walk($options, array($this, 'resvoleOptions'));
+        \array_walk($options, array($this, 'resolveOptions'));
 
         return $this;
     }
@@ -49,31 +52,24 @@ class Element {
      */
     public function set(array $options): Element
     {
-        $this->element = $this->value = $this->hasClass = null;
+        $this->element = $this->value = $this->hasClass = $this->selector = null;
 
-        array_walk($options, array($this, 'resvoleOptions'));
+        \array_walk($options, array($this, 'resolveOptions'));
 
         return $this;
     }
 
-    private function resvoleOptions($value, $key)
+    private function resolveOptions($value, $key)
     {
         switch($key){
             case 'selector':
-                try {
-                    if($this->element == null) {
-                        $this->element = $this->navigation->webDriver->findElement(WebDriverBy::cssSelector($value));
-                    } else {
-                        $this->element = $this->element->findElement(WebDriverBy::cssSelector($value));
-                    }
-                } catch (Exception $e) {
-                    $this->element = null;
-                }
-            break;
+                $this->selector = $value;
+                $this->getElementBySelector($value);
+                break;
 
             case 'element':
                 $this->element = $value;
-            break;
+                break;
 
             case 'attribute':
                 if($this->element == null)
@@ -82,32 +78,59 @@ class Element {
                 } else {
                     $this->value = ($value == 'text') ? $this->element->getText() : $this->element->getAttribute($value);
                 }
-            break;
+                break;
 
             case 'click':
                 ($value === true) ? $this->element->click() : null;
-            break;
+                break;
 
             case 'class':
                 $classes = $this->element->getAttribute('class');
                 $this->hasClass = in_array($value, explode(' ', $classes));
-            break;
+                break;
 
             case 'input':
                 $this->element->sendKeys($value);
-            break;
+                break;
 
             case 'pressKey':
-                $ref = new ReflectionClass('Facebook\WebDriver\WebDriverKeys');
+                $ref = new \ReflectionClass('Facebook\WebDriver\WebDriverKeys');
                 $this->element->sendKeys($ref->getConstant($value));
-            break;
+                break;
 
             case 'delay':
                 $this->navigation->sleep($value);
-            break;
+                break;
+
+            case 'waitForElement':
+                $i = 0;
+                while($this->element == null){
+                    $i++;
+                    if($i > $value)
+                        break;
+                    $this->navigation->sleep(10000);
+                    $this->getElementBySelector($this->selector);
+                }
+                break;
         }
 
-        return null;
+        return;
+    }
+
+    private function getElementBySelector(string $selector): bool
+    {
+        try {
+            if($this->element == null) {
+                $this->element = $this->navigation->webDriver->findElement(WebDriverBy::cssSelector($selector));
+            } else {
+                $this->element = $this->element->findElement(WebDriverBy::cssSelector($selector));
+            }
+            return true;
+        } catch (\Exception $e) {
+            $this->element = null;
+
+        }
+        return false;
     }
 
     public function getValue($returnType = null)
@@ -119,16 +142,16 @@ class Element {
         switch($returnType){
             case 'int':
                 return Filter::int((string) $this->value);
-            break;
+                break;
 
             case 'float':
                 return Filter::float((string) $this->value);
-            break;
+                break;
 
             case null:
             default:
                 return $this->value;
-            break;
+                break;
         }
     }
 }
