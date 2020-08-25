b3a4ab253c37dde64120e1ea74c081678f638ba4
Daniel Westerling <daniel@westerling.io>
2020-08-25 14:30:52 +0200

    Reformatted Code into Wordpress Code Style.

--- src/Element.php
+++ src/Element.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare( strict_types=1 );
 
 namespace Akdr\Selma;
 
@@ -12,192 +12,185 @@ use ReflectionClass;
 use function array_walk;
 
 class Element {
-    /**
-     * @var Navigation
-     */
-    private $navigation;
+	/**
+	 * @var Navigation
+	 */
+	private $navigation;
 
-    /**
-     * @var RemoteWebElement
-     */
-    public $element;
+	/**
+	 * @var RemoteWebElement
+	 */
+	public $element;
 
-    /**
-     * @var mixed
-     */
-    private $value;
+	/**
+	 * @var mixed
+	 */
+	private $value;
 
-    /**
-     * @var string
-     */
-    private $selector;
+	/**
+	 * @var string
+	 */
+	private $selector;
 
-    /**
-     * @var bool
-     */
-    public $hasClass;
+	/**
+	 * @var bool
+	 */
+	public $hasClass;
 
-    /**
-     * Element constructor.
-     * @param Navigation $navigation
-     * @param array $options
-     */
-    public function __construct(Navigation $navigation, array $options)
-    {
-        $this->navigation = $navigation;
-        // Walk through the options array and set variables in the class.
-        // Selector must be the first option in the array.
-        array_walk($options, array($this, 'resolveOptions'));
+	/**
+	 * Element constructor.
+	 *
+	 * @param Navigation $navigation
+	 * @param array $options
+	 */
+	public function __construct( Navigation $navigation, array $options ) {
+		$this->navigation = $navigation;
+		// Walk through the options array and set variables in the class.
+		// Selector must be the first option in the array.
+		array_walk( $options, array( $this, 'resolveOptions' ) );
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Keys are: selector (non-optional), element, attribute, click, class, input, pressKey, delay
-     * Set also resets saved values.
-     * @return Element
-     * @var array $options Array will be initiated in the order presented.
-     */
-    public function set(array $options): Element
-    {
-        $this->element = $this->value = $this->hasClass = $this->selector = null;
+	/**
+	 * Keys are: selector (non-optional), element, attribute, click, class, input, pressKey, delay
+	 * Set also resets saved values.
+	 * @return Element
+	 * @var array $options Array will be initiated in the order presented.
+	 */
+	public function set( array $options ): Element {
+		$this->element = $this->value = $this->hasClass = $this->selector = null;
 
-        array_walk($options, array($this, 'resolveOptions'));
+		array_walk( $options, array( $this, 'resolveOptions' ) );
 
-        return $this;
-    }
+		return $this;
+	}
 
-    private function resolveOptions($value, $key)
-    {
-        switch($key){
-            case 'selector':
-                $this->selector = $value;
-                $this->getElementBySelector($value);
-                break;
+	private function resolveOptions( $value, $key ) {
+		switch ( $key ) {
+			case 'selector':
+				$this->selector = $value;
+				$this->getElementBySelector( $value );
+				break;
 
-            case 'element':
-                $this->element = $value;
-                break;
+			case 'element':
+				$this->element = $value;
+				break;
 
-            case 'attribute':
-                if($this->element == null)
-                {
-                    $this->value = null;
-                } else {
-                    $this->value = ($value == 'text') ? $this->element->getText() : $this->element->getAttribute($value);
-                }
-                break;
+			case 'attribute':
+				if ( $this->element == null ) {
+					$this->value = null;
+				} else {
+					$this->value = ( $value == 'text' ) ? $this->element->getText() : $this->element->getAttribute( $value );
+				}
+				break;
 
-            case 'click':
-                if($this->element == null && $value === true)
-                {
-                    error_log('Could not click on element because it is not selected. Selector: ' . $this->selector);
-                    break;
-                }
-                ($value === true) ? $this->element->click() : null;
-                break;
+			case 'click':
+				if ( $this->element == null && $value === true ) {
+					error_log( 'Could not click on element because it is not selected. Selector: ' . $this->selector );
+					break;
+				}
+				( $value === true ) ? $this->element->click() : null;
+				break;
 
-            case 'class':
-                $classes = $this->element->getAttribute('class');
-                $this->hasClass = in_array($value, explode(' ', $classes));
-                break;
+			case 'class':
+				$classes        = $this->element->getAttribute( 'class' );
+				$this->hasClass = in_array( $value, explode( ' ', $classes ) );
+				break;
 
-            case 'input':
-                $this->element->sendKeys($value);
-                break;
+			case 'input':
+				$this->element->sendKeys( $value );
+				break;
 
-            case 'pressKey':
-                $ref = new ReflectionClass('Facebook\WebDriver\WebDriverKeys');
-                $this->element->sendKeys($ref->getConstant($value));
-                break;
+			case 'pressKey':
+				$ref = new ReflectionClass( 'Facebook\WebDriver\WebDriverKeys' );
+				$this->element->sendKeys( $ref->getConstant( $value ) );
+				break;
 
-            case 'delay':
-                $this->navigation->sleep($value);
-                break;
+			case 'delay':
+				$this->navigation->sleep( $value );
+				break;
 
-            case 'waitForElement':
-                $i = 0;
-                while($this->element == null){
-                    $i++;
-                    if($i > $value)
-                        break;
-                    $this->navigation->sleep(10000);
-                    $this->getElementBySelector($this->selector);
-                }
-                break;
-        }
+			case 'waitForElement':
+				$i = 0;
+				while ( $this->element == null ) {
+					$i ++;
+					if ( $i > $value ) {
+						break;
+					}
+					$this->navigation->sleep( 10000 );
+					$this->getElementBySelector( $this->selector );
+				}
+				break;
+		}
+	}
 
-        return;
-    }
+	private function getElementBySelector( string $selector ): bool {
+		try {
+			if ( $this->element == null ) {
+				$this->element = $this->navigation->webDriver->findElement( WebDriverBy::cssSelector( $selector ) );
+			} else {
+				$this->element = $this->element->findElement( WebDriverBy::cssSelector( $selector ) );
+			}
 
-    private function getElementBySelector(string $selector): bool
-    {
-        try {
-            if($this->element == null) {
-                $this->element = $this->navigation->webDriver->findElement(WebDriverBy::cssSelector($selector));
-            } else {
-                $this->element = $this->element->findElement(WebDriverBy::cssSelector($selector));
-            }
-            return true;
-        } catch (Exception $e) {
-            $this->element = null;
+			return true;
+		} catch ( Exception $e ) {
+			$this->element = null;
 
-        }
-        return false;
-    }
+		}
 
-    public function getValue($returnType = null)
-    {
-        if(is_null($this->value)){
-            return null;
-        }
+		return false;
+	}
 
-        switch($returnType){
-            case 'int':
-                $returnValue = Filter::int((string) $this->value);
-                break;
-            case 'float':
-                $returnValue = Filter::float((string) $this->value);
-                break;
-            case null:
-            default:
-                $returnValue = $this->value;
-        }
+	public function getValue( $returnType = null ) {
+		if ( is_null( $this->value ) ) {
+			return null;
+		}
 
-        if(empty($returnValue)){
-            error_log('Selector: ' . $this->selector . ' had a value of ' . $returnValue . ' (Preformatted: ' . $this->value .')');
-        }
+		switch ( $returnType ) {
+			case 'int':
+				$returnValue = Filter::int( (string) $this->value );
+				break;
+			case 'float':
+				$returnValue = Filter::float( (string) $this->value );
+				break;
+			case null:
+			default:
+				$returnValue = $this->value;
+		}
 
-        return $returnValue;
-    }
+		if ( empty( $returnValue ) ) {
+			error_log( 'Selector: ' . $this->selector . ' had a value of ' . $returnValue . ' (Preformatted: ' . $this->value . ')' );
+		}
 
-    public function findElement($selector): ?RemoteWebElement
-    {
-        try {
-            $remoteWebElement = $this->navigation->webDriver->findElement(WebDriverBy::cssSelector($selector));
-        } catch (Exception $e) {
-            $remoteWebElement = null;
-        }
-        return $remoteWebElement;
-    }
+		return $returnValue;
+	}
 
-    public function findElements($selector): ?array
-    {
-        try {
-            $remoteWebElements = $this->navigation->webDriver->findElements(WebDriverBy::cssSelector($selector));
-        } catch (Exception $e) {
-            $remoteWebElements = null;
-        }
+	public function findElement( $selector ): ?RemoteWebElement {
+		try {
+			$remoteWebElement = $this->navigation->webDriver->findElement( WebDriverBy::cssSelector( $selector ) );
+		} catch ( Exception $e ) {
+			$remoteWebElement = null;
+		}
 
-        return $remoteWebElements;
-    }
+		return $remoteWebElement;
+	}
 
-    public function grabSelectorValue($selector, $returnType = null, $attribute = null)
-    {
-        $this->selector = $selector;
-        $element = $this->navigation->webDriver->findElement(WebDriverBy::cssSelector($this->selector));
-        $this->value = ($attribute == 'text' || is_null($attribute)) ? $element->getText() : $element->getAttribute($attribute);
+	public function findElements( $selector ): ?array {
+		try {
+			$remoteWebElements = $this->navigation->webDriver->findElements( WebDriverBy::cssSelector( $selector ) );
+		} catch ( Exception $e ) {
+			$remoteWebElements = null;
+		}
 
-        return $this->getValue($returnType);
-    }
+		return $remoteWebElements;
+	}
+
+	public function grabSelectorValue( $selector, $returnType = null, $attribute = null ) {
+		$this->selector = $selector;
+		$element        = $this->navigation->webDriver->findElement( WebDriverBy::cssSelector( $this->selector ) );
+		$this->value    = ( $attribute == 'text' || is_null( $attribute ) ) ? $element->getText() : $element->getAttribute( $attribute );
+
+		return $this->getValue( $returnType );
+	}
 }
--- src/Navigation.php
+++ src/Navigation.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare( strict_types=1 );
 
 namespace Akdr\Selma;
 
@@ -8,123 +8,122 @@ use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 
-class Navigation
-{
-    /**
-     * @var RemoteWebDriver
-     */
-    public $webDriver;
+class Navigation {
+	/**
+	 * @var RemoteWebDriver
+	 */
+	public $webDriver;
 
-    /**
-     * @var string
-     * @var array;
-     */
-    public function __construct(string $webdriverHostURL, array $chromeOptionsArguments)
-    {
-        $options = new ChromeOptions();
-        $options->addArguments($chromeOptionsArguments);
+	/**
+	 * @var string
+	 * @var array;
+	 */
+	public function __construct( string $webdriverHostURL, array $chromeOptionsArguments ) {
+		$options = new ChromeOptions();
+		$options->addArguments( $chromeOptionsArguments );
 
-        $capabilities = DesiredCapabilities::chrome();
-        $capabilities->setCapability(ChromeOptions::CAPABILITY_W3C, $options);
-        $capabilities->setPlatform("Linux");
-        $this->webDriver = RemoteWebDriver::create($webdriverHostURL, $capabilities);
-    }
+		$capabilities = DesiredCapabilities::chrome();
+		$capabilities->setCapability( ChromeOptions::CAPABILITY_W3C, $options );
+		$capabilities->setPlatform( "Linux" );
+		$this->webDriver = RemoteWebDriver::create( $webdriverHostURL, $capabilities );
+	}
 
-    /**
-     * Make the driver browse a url
-     *
-     * @param string $url
-     *
-     * @return Navigation
-     */
-    public function goTo(string $url): Navigation
-    {
-        $this->webDriver->get($url);
-        return $this;
-    }
+	/**
+	 * Make the driver browse a url
+	 *
+	 * @param string $url
+	 *
+	 * @return Navigation
+	 */
+	public function goTo( string $url ): Navigation {
+		$this->webDriver->get( $url );
 
-    /**
-     * Return the URL which the browser is currently showing
-     *
-     * @return string
-     */
-    public function currentUrl(): string
-    {
-        return $this->webDriver->getCurrentURL();
-    }
+		return $this;
+	}
 
-    /**
-     * Executes javascript in the browser and returns the answer if able to.
-     *
-     * @param string $script
-     * @return null|string
-     */
-    public function javascript(string $script)
-    {
-        return $this->webDriver->executeScript($script);
-    }
+	/**
+	 * Return the URL which the browser is currently showing
+	 *
+	 * @return string
+	 */
+	public function currentUrl(): string {
+		return $this->webDriver->getCurrentURL();
+	}
 
-    /**
-     * sleep
-     *
-     * @param int $ms
-     * @return Navigation
-     */
-    public function sleep(int $ms = 300000): Navigation
-    {
-        usleep($ms);
-        return $this;
-    }
+	/**
+	 * Executes javascript in the browser and returns the answer if able to.
+	 *
+	 * @param string $script
+	 *
+	 * @return null|string
+	 */
+	public function javascript( string $script ) {
+		return $this->webDriver->executeScript( $script );
+	}
 
-    /**
-     * Make the Selenium browser scroll to a specific height. If no param is set, it will scroll all the way down.
-     * @param int|null $scrollHeight
-     * @return Navigation
-     */
-    public function scrollTo(?int $scrollHeight = null): Navigation
-    {
-        if ($scrollHeight == null) {
-            $this->webDriver->executeScript('window.scrollTo(0,document.body.scrollHeight);');
-        } else {
-            $this->webDriver->executeScript("window.scrollTo(0,$scrollHeight);");
-        }
+	/**
+	 * sleep
+	 *
+	 * @param int $ms
+	 *
+	 * @return Navigation
+	 */
+	public function sleep( int $ms = 300000 ): Navigation {
+		usleep( $ms );
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * screenshot
-     *
-     * @param string $absolutPath
-     * @return Navigation
-     */
-    public function screenshot(string $absolutPath): Navigation
-    {
-        $this->webDriver->takeScreenshot($absolutPath);
-        $this->cli('Screenshot taken on ' . $this->currentUrl());
-        return $this;
-    }
+	/**
+	 * Make the Selenium browser scroll to a specific height. If no param is set, it will scroll all the way down.
+	 *
+	 * @param int|null $scrollHeight
+	 *
+	 * @return Navigation
+	 */
+	public function scrollTo( ?int $scrollHeight = null ): Navigation {
+		if ( $scrollHeight == null ) {
+			$this->webDriver->executeScript( 'window.scrollTo(0,document.body.scrollHeight);' );
+		} else {
+			$this->webDriver->executeScript( "window.scrollTo(0,$scrollHeight);" );
+		}
 
-    public function getSource(): string
-    {
-        return $this->webDriver->getPageSource();
-    }
+		return $this;
+	}
 
-    /**
-     *
-     * @param string $message
-     * @return Navigation
-     */
-    public function cli(string $message) : Navigation
-    {
-        error_log($message);
-        return $this;
-    }
+	/**
+	 * screenshot
+	 *
+	 * @param string $absolutPath
+	 *
+	 * @return Navigation
+	 */
+	public function screenshot( string $absolutPath ): Navigation {
+		$this->webDriver->takeScreenshot( $absolutPath );
+		$this->cli( 'Screenshot taken on ' . $this->currentUrl() );
 
-    public function __destruct()
-    {
-        $this->screenshot('/var/app/mobiltelefoner/storage/exit.png');
-        error_log('Killing Browser on exit');
-        $this->webDriver->quit();
-    }
+		return $this;
+	}
+
+	public function getSource(): string {
+		return $this->webDriver->getPageSource();
+	}
+
+	/**
+	 *
+	 * @param string $message
+	 *
+	 * @return Navigation
+	 */
+	public function cli( string $message ): Navigation {
+		error_log( $message );
+
+		return $this;
+	}
+
+	public function __destruct() {
+		$this->screenshot( '/var/app/mobiltelefoner/storage/exit.png' );
+		error_log( 'Killing Browser on exit' );
+		$this->webDriver->quit();
+	}
 }
