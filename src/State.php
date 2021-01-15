33d080fbfedfc76d48c1b8a2957e853c1321e85f
Akdr <daniel.olov.westerling@gmail.com>
2021-01-15 13:09:18 +0100

    feat: adding string comparison over time

--- src/State.php
+++ src/State.php
@@ -29,6 +29,7 @@ class State {
 	 * Element constructor.
 	 *
 	 * @param Navigation $navigation
+	 * @param null|string $originalValue
 	 */
 	public function __construct( Navigation $navigation, $originalValue = null ) {
 		$this->navigation = $navigation;
@@ -39,6 +40,14 @@ class State {
 		return $this;
 	}
 
+	/**
+	 * @param $selector
+	 * @param string $attribute
+	 * @param int $waitForInSeconds
+	 * @param null $element
+	 *
+	 * @return bool|null
+	 */
 	public function observeAttributeChange($selector, $attribute = 'text', $waitForInSeconds = 30, $element = null): ?bool
 	{
 		if($selector === null || !$this->element->findElement($selector) )
@@ -86,6 +95,12 @@ class State {
 		return $originalState !== $changedState;
 	}
 
+	/**
+	 * @param $selector
+	 * @param int $waitForInSeconds
+	 *
+	 * @return bool|null
+	 */
 	public function observeDOMChange($selector, $waitForInSeconds = 30): ?bool
 	{
 		if($selector === null )
@@ -114,6 +129,58 @@ class State {
 		return $originalState !== $observedState;
 	}
 
+
+	/**
+	 * @param string $originalString
+	 * @param callable $newStringFunction
+	 * @param callable $returnFunction
+	 * @param int $waitForInSeconds
+	 *
+	 * @return bool
+	 */
+	public function observeStringChange(string $originalString,
+		callable $newStringFunction,
+		callable $returnFunction,
+		int $waitForInSeconds = 30): bool {
+
+		// Get the function calling for the new value that we compare with the original string.
+		$newString = $newStringFunction();
+
+		// Get the current time in seconds
+		$currentTime = microtime(true);
+
+		// Add the time specified in $waitForInSeconds
+		$exitTime = $currentTime + $waitForInSeconds;
+
+
+		// While the original string is the same as the new, we continue the loop
+		// If the time has passed specified in $waitForInSeconds, abort and return the comparison
+		// at the bottom
+		while($originalString === $newString){
+			// Check the time again
+			$currentTime = microtime(true);
+
+			// Compare the time
+			if ( $currentTime > $exitTime ) {
+				// Input a error message to the terminal
+				error_log( 'No update for the string ' . $originalString
+				           . ' was found in ' . $waitForInSeconds . ' seconds.');
+				break;
+			}
+			// Update the new string value
+			$newString = $newStringFunction();
+		}
+
+		// Return true or false
+		return $returnFunction($originalString, $newString);
+	}
+
+	/**
+	 * @param $originalState
+	 * @param int $waitForInSeconds
+	 *
+	 * @return bool|null
+	 */
 	public function observeURLChange($originalState, $waitForInSeconds = 30): ?bool
 	{
 		if($originalState === null )
@@ -125,7 +192,6 @@ class State {
 		$exitTime = $currentTime + $waitForInSeconds;
 
 		$observedState = $this->originalValue;
-
 		while($observedState === $originalState)
 		{
 			$currentTime = microtime(true);
@@ -138,10 +204,12 @@ class State {
 			$observedState = $this->navigation->currentUrl();
 
 		}
-
 		return $observedState !== $originalState;
 	}
 
+	/**
+	 * @param array $selectorArray
+	 */
 	public function setMultipleDomValues(array $selectorArray): void
 	{
 		foreach($selectorArray as $selector){
@@ -150,6 +218,12 @@ class State {
 		return;
 	}
 
+	/**
+	 * @param array $selectorArray
+	 * @param int $waitForInSeconds
+	 *
+	 * @return string|null
+	 */
 	public function observeMultipleDOM(array $selectorArray, int $waitForInSeconds): ?string
 	{
 		if(empty($selectorArray))
